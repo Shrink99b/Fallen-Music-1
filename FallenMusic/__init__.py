4cf007ca73c896d5594011c90e125de510d34122
Shrink99b <115853073+Shrink99b@users.noreply.github.com>
2022-11-10 14:13:22 +0530

    Update __init__.py

--- FallenMusic/__init__.py
+++ FallenMusic/__init__.py
@@ -59,7 +59,6 @@ async def fallen_boot():
     global BOT_ID, BOT_NAME, BOT_USERNAME
     global ASSID, ASSNAME, ASSMENTION, ASSUSERNAME
     os.system("clear")
-    ) as status:
         console.print("┌ [red]Booting Fallen Music Clients...\n")
         await app.start()
         await Ass.start()
